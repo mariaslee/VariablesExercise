0d2c0afac5099b548d6d6d41f332c56149ff1a24
marias lee <marias_lee84@yahoo.com>
2022-08-05 15:25:13 -0500

    intial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variables_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cat = "Florida cat";
+            int age = 3;
+            char exclamation = '!';
+            bool quick = false;
+            double lives = 5.5;
+            decimal feet = 5m;
+
+            Console.WriteLine($"Hello {cat} !");
+            Console.WriteLine($"My cat is {age} years old");
+            Console.WriteLine($"He sings very well {exclamation} ");
+            Console.WriteLine($"My cat thinks he's fast but that's very {quick} .");
+            Console.WriteLine($"He has about {lives} lives left.");
+            Console.WriteLine($"My cat can jump about {feet} feet high. ");
+        }
+    }
+}
